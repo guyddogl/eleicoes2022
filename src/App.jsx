bfec814d20bb09f1325b570a31f0dde63bfdbb44
guyddogl <guyddogl@gmail.com>
2022-10-29 09:41:59 -0300

    Fetch data from TSE Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import getCurrentDateTime from './utils/date';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [dataTSE, setDataTSE] = useState({});
+
+  useEffect(() => {
+    const URL = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json';
+    const getDataTSE = async () => {
+      await fetch(URL)
+        .then((response) => response.json())
+        .then((data) => setDataTSE(data));
+      setIsLoading(false);
+    };
+    getDataTSE();
+  }, []);
+
+  console.log(dataTSE);
   return (
     <div>
+      {isLoading && <p>Loading...</p>}
       <p>
         Última atualização:
         {' '}
