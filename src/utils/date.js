9a2e19ac88ccef8aab714514ebc52483b0759672
guyddogl <guyddogl@gmail.com>
2022-10-29 09:39:04 -0300

    Create getCurrentDateTime

---
+++ src/utils/date.js
@@ -0,0 +1,12 @@
+const getCurrentDateTime = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
+  const minute = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
+  const second = date.getSeconds() < 10 ? `0${date.getSeconds()}` : `${date.getSeconds()}`;
+  return `${day}/${month}/${year} ${hour}:${minute}:${second}`;
+};
+
+export default getCurrentDateTime;
